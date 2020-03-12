d4d42e880fb06e28dab3ddcdd98dd2503b2915b4
Kleeer <delhomme.claire.ts7@gmail.com>
2020-03-12 14:08:10 +0100

    base saine pour modif resize

--- Matrix.h
+++ Matrix.h
@@ -103,13 +103,56 @@ private:
     static bool isSeedInit;
 
 
+    /**
+     *  Opération effectuée en modifiant la matrice sur laquelle est invoquée la méthode,
+     *
+     * @param matrix    l'autre matrice dans l'opération
+     * @param op        l'opérateur à appliquer
+     */
     void operationSelf(const Matrix& matrix, const Operator& op);
+    /**
+     *  Opération effectuée en retournant par valeur une nouvelle matrice résultat allouée statiquement
+     *
+     * @param matrix    l'autre matrice dans l'opération
+     * @param op        l'opérateur à appliquer
+     * @return  par valeur une nouvelle matrice résultat allouée statiquement
+     */
     Matrix operationStatic(const Matrix& matrix, const Operator& op) const;
+    /**
+     *  Opération effectuée en retournant un pointeur sur une nouvelle matrice résultat allouée dynamiquement
+     *
+     * @param matrix    l'autre matrice dans l'opération
+     * @param op        l'opérateur à appliquer
+     * @return  un pointeur sur une nouvelle matrice résultat allouée dynamiquement
+     */
     Matrix* operationDynamic(const Matrix& matrix, const Operator& op) const ;
 
+    /**
+     *  Vérifie que deux matrices ont le même modulo
+     *
+     * @param matrix    l'autre matrice dans l'opération
+     */
     void checkModulo(const Matrix& matrix) const noexcept(false);
+    /**
+     *  Redimensionne la matrice sur laquelle est appelée la méthode aux dimensions de l'autre matrice si
+     *  cette dernière est plus grande
+     *
+     * @param matrix    l'autre matrice dans l'opération
+     */
     void resize(const Matrix& matrix);
+    /**
+     *
+     * @param value la valeur sur laquelle appliquer le modulo
+     * @param m     le modulo
+     * @return  le modulo positif de value
+     */
     unsigned applyModulo(int value, unsigned m) const;
+    /**
+     *  Redimensionne la matrice appelante et applique
+     *
+     * @param matrix    l'autre matrice dans l'opération
+     * @param op        l'opérateur à appliquer
+     */
     void computeMatrix(const Matrix& matrix, const Operator& op);
 
 };
